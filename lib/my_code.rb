bbc66cdaf80796a5c61c7100b37a9569df3680e3
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-03 13:55:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@
 def map_to_negativize(source_array)
   i = 0
   array_result = []
-  while i < array.length do
+  while i < source_array.length do
     array_result.push(source_array[i] * -1)
   end
   return array_result
